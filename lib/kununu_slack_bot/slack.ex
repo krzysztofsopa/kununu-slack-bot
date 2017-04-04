e1ea96fc0d6fa2cfa18feffcf0b009aa262d45b0
Franz B <franz.buchsbaum@kununu.com>
2017-04-04 14:51:30 +0200

    Log username >:>

--- lib/kununu_slack_bot/slack.ex
+++ lib/kununu_slack_bot/slack.ex
@@ -2,22 +2,34 @@ defmodule KununuSlackBot.Slack do
   use Slack
   require Logger
 
+  def get_user_info(user_id) do
+    cond do
+      %{"ok" => true, "user" => user} = Slack.Web.Users.info(user_id) ->
+        {:ok, user}
+    end
+  end
+
+  def get_user_name(user_id) do
+    cond do
+      {:ok, user} = get_user_info(user_id) ->
+        {:ok, user["name"]}
+    end
+  end
+
   def handle_connect(slack, state) do
     Logger.info "Connected as #{slack.me.name}!"
 
-    names = Slack.Web.Users.list()
-    |> Map.get("members")
-    |> Enum.map(fn(member) ->
-      member["real_name"]
-    end)
-
-    Logger.info "#{inspect(names)}"
-    
     {:ok, state}
   end
 
   def handle_event(message = %{type: "message"}, slack, state) do
-    Logger.info "Got a message: #{inspect(message)}"
+    if message.user == slack.me.id do
+      Logger.debug "Got a message from myself? 0o"
+    end
+
+    {:ok, username} = get_user_name(message.user)
+
+    Logger.info "Got a message from @#{username}: #{inspect(message.text)}"
 
     {:ok, response} = HTTPoison.get("https://www.kununu.com/at/partner/KlM=/json", [], [ ssl: [{:versions, [:'tlsv1.2']}] ])
     {status, o} = JSON.decode(response.body)
@@ -36,7 +48,7 @@ defmodule KununuSlackBot.Slack do
   def handle_info({:message, text, channel}, slack, state) do
     Logger.info "Sending your message to #{channel}!"
 
-    #send_message(text, channel, slack)
+    send_message(text, channel, slack)
 
     {:ok, state}
   end
