8bf2d4748ec44864cfc2eb9295c874507cfaf161
Franz B <franz.buchsbaum@kununu.com>
2017-04-04 10:44:37 +0200

    Add slack to used applications

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule KununuSlackBot.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :slack],
      mod: {KununuSlackBot.Application, []}]
   end
 
